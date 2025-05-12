a679ccd957aa37326144949d496128a36a4b08fc
Melvin-Emmanuel <74471443+Melvin-Emmanuel@users.noreply.github.com>
2025-05-12 14:47:50 +0000

    connect db

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 // src/index.js
 const express = require('express');
 const app = express();
+const connectDB = require('./config/db');
+connectDB();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
